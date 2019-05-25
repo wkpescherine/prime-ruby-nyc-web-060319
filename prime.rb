1a0a9ffa274cd2068a08a842d640345446cd480e
William Pescherine <wkpescherine@gmail.com>
2019-05-25 04:07:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(number)
+  all_numbers_array = []
+  current_count = 0
+  is_number_prime = false
+  
+  while current_count < number
+    all_numbers_array.push(current_count+1)
+    current_count += 1
+  end
+  
+  if number <= 1
+    is_number_prime = false
+  elsif number ==2
+    is_number_prime = true
+  else
+    all_numbers_array.each do |divide|
+    
+  end
+  
+  is_number_prime
+end
