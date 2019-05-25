692bf1252ceeddddf3ee362249048a2c607f38ec
William Pescherine <wkpescherine@gmail.com>
2019-05-25 04:14:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,9 @@ def prime?(number)
   elsif number ==2
     is_number_prime = true
   else
-    all_numbers_array.each do |divide|
-    if number%divide = 0
-      
-    end  
+    (2...num).each do |divisor|
+      return false if num % divisor == 0
+    end 
   end
   
   is_number_prime
