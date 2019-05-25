2d4aa25ec8d66711735d549379df857deef29416
William Pescherine <wkpescherine@gmail.com>
2019-05-25 04:19:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  is_number_prime = true
+  
+  if number <= 1
+    is_number_prime = false
+  elsif number ==2
+    is_number_prime = true
+  else
+    (2...number).each do |divisor|
+      return false if number % divisor == 0
+    end 
+  end
+  
+  is_number_prime
+end
